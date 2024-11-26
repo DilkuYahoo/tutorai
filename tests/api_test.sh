d0f6537aa263e2d552494e365a3a2343dd4232d7
Dilku Karunaratne <dilku@yahoo.com>
2024-11-26 23:24:01 +1100

    Fixed the loading issue by using B4 instance type

--- tests/api_test.sh
+++ tests/api_test.sh
@@ -2,7 +2,8 @@
 
 # Define the API endpoint and base URL
 #API='multiply'
-API='ticker_analysis'
+#API='ticker_analysis'
+API='sentiment_tracker'
 #URL='http://localhost:8080'
 # Uncomment and set the desired URL
 URL='https://fintelle.wn.r.appspot.com'
@@ -14,6 +15,9 @@ FULL_URL="${URL}/${API}"
 # Make the POST request with curl
 #curl -X POST "$FULL_URL" -H "Content-Type: application/json" -d '{"a": "12", "b": "15"}'
 
-curl -X POST "$FULL_URL" -H "Content-Type: application/json" -d '{"TickerSymbol": "ANZ", "exchangeName": ".ax", "period": "1d"}'
+#curl -X POST "$FULL_URL" -H "Content-Type: application/json" -d '{"TickerSymbol": "GEM", "exchangeName": ".ax", "period": "3mo"}'
+
+curl -X POST "$FULL_URL" -H "Content-Type: application/json" -d '{"TickerSymbol": "ANZ", "exchangeName": ".ax"}'
+
 # Uncomment the following line to use a specific URL without concatenation
 # curl -X POST "$URL" -H "Content-Type: application/json" -d '{"a": "12", "b": "15"}'
